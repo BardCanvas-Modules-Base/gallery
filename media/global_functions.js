960728c04d47c29a7faaf7d32379aa6dbafa10a7
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-18 22:58:02 -0500

    Reworked post editor buttons

--- media/global_functions.js
+++ media/global_functions.js
@@ -1,4 +1,17 @@
 
+// TODO: Move stuff here to a forging API
+
+if( typeof $_POST_ADDON_FUNCTIONS == 'undefined' )
+    var $_POST_ADDON_FUNCTIONS = {};
+
+$_POST_ADDON_FUNCTIONS['insert_gallery_image_in_post_editor'] = function($trigger, $form)
+{
+    var editor_id = $form.find('textarea.tinymce').attr('id');
+    var editor    = tinymce.get(editor_id);
+    
+    load_media_browser_in_tinymce_dialog(editor, $(window).width() - 20, $(window).height() - 60, 'image');
+};
+
 function load_media_browser_in_tinymce_dialog(editor, width, height, media_type_filter)
 {
     var $messages = $('#gallery_strings_for_tinymce');
