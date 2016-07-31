22130b8cbffb4f4b7282ca2b171e85792ce823e3
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-31 07:31:47 -0500

    Implemented user profile tabs

--- by_author.php
+++ by_author.php
@@ -24,7 +24,17 @@ $author = new account($_GET["slug"]);
 if( ! $author->_exists ) throw_fake_404();
 
 $type = empty($_GET["type"]) ? "any" : $_GET["type"];
+switch($_GET["type"])
+{
+    case "video": $tab = "videos"; break;
+    case "image": $tab = "images"; break;
+    default:      $tab = "media";  break;
+}
+
 $template->set("showing_archive", true);
+$template->set("show_user_profile_heading", true);
+$template->set("user_profile_account", $author);
+$template->set("current_user_profile_tab", $tab);
 $template->page_contents_include = "by_author.inc";
 $template->set_page_title(replace_escaped_vars(
     $current_module->language->pages->by_author->title,
