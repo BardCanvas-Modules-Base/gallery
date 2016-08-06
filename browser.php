29a2f86ce2aea08974af523b5b414e3470d4433d
Alejandro Caballero <lava.caballero@gmail.com>
2016-08-06 17:42:58 -0500

    Fixed permissions issue

--- browser.php
+++ browser.php
@@ -6,15 +6,21 @@
  * @subpackage gallery
  * @author     Alejandro Caballero - lava.caballero@gmail.com
  *
+ * @var account  $account
+ * @var template $template
+ * 
  * $_GET params
  * @param embedded_mode
  * @param callback
  * @param search_type
  */
 
+use hng2_base\account;
+use hng2_base\template;
+
 include "../config.php";
 include "../includes/bootstrap.inc";
-if( ! $account->_is_admin ) throw_fake_404();
+if( ! $account->_exists ) throw_fake_404();
 
 $template->page_contents_include = "contents/browser.inc";
 $template->set_page_title($current_module->language->index->title);
--- index.php
+++ index.php
@@ -5,15 +5,22 @@
  * @package    BardCanvas
  * @subpackage gallery
  * @author     Alejandro Caballero - lava.caballero@gmail.com
- *             
+ * 
+ * @var account  $account
+ * @var template $template
+ * 
  * $_GET params
  * @param embedded_mode
  * @param callback
  * @param search_type
  */
 
+use hng2_base\account;
+use hng2_base\template;
+
 include "../config.php";
 include "../includes/bootstrap.inc";
+if( ! $account->_exists ) throw_fake_404();
 
 $template->set_page_title($current_module->language->index->title);
 
