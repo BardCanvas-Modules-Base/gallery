59bacf65d64e5a2567a81657439e6d01940d97f9
Alejandro Caballero <lava.caballero@gmail.com>
2016-08-08 01:09:39 -0500

    Added missing template sets and tuned existing ones

--- by_category.php
+++ by_category.php
@@ -25,9 +25,12 @@ if( empty($_GET["slug"]) ) throw_fake_404();
 $category = $categories_repository->get($_GET["slug"]);
 if( is_null($category) ) throw_fake_404();
 
-$template->set("page_tag", "media_category_index");
-$template->set("showing_archive", true);
-$template->set("current_category_slug", $category->slug);
+$template->set("page_tag",               "media_category_index");
+$template->set("showing_archive",        true);
+$template->set("current_category_slug",  $category->slug);
+$template->set("current_category_title", $category->title);
+$template->set("current_category_id",    $category->id_category);
+
 $template->page_contents_include = "by_category.inc";
 $template->set_page_title(replace_escaped_vars(
     $current_module->language->pages->by_category->title, '{$category}', $category->title
